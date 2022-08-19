437a8fa23fa56bba6fab7eb471191ab0936484e7
Gabi Castillo <109112189+gabriellepcastillo@users.noreply.github.com>
2022-08-19 11:38:30 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,102 +1,93 @@
  // Assignment code here
-var characterTypes = function () {
-    passwordPromts.lowercase = window.prompt("Would you like to include lowercase characters? Yes or No?");
-    passwordPromts.lowercase = passwordPromts.lowercase.toLowerCase ();
-    if (passwordPromts.lowercase === "yes") {
-        window.alert ("You have choosen to include lowercase characters.");
-        passwordPromts.lowercase = true
-    }
-    else if (passwordPromts.lowercase === "no") {
-        window.alert ("You have choosen not to include lowercase characters.")
-        passwordPromts.lowercase = false
-    }
-   
-    passwordPromts.uppercase = window.prompt("Would you like to include uppercase characters? Yes or No?");
-    passwordPromts.uppercase = passwordPromts.uppercase.toLowerCase ();
-    if (passwordPromts.uppercase === "yes") {
-        window.alert ("You have choosen to include uppercase characters.");
-        passwordPromts.uppercase = true
-    }
-    else if (passwordPromts.uppercase === "no") {
-        window.alert ("You have choosen not to include uppercase characters.")
-        passwordPromts.uppercase = false
-    }
-
-    passwordPromts.numeric = window.prompt("Would you like to include numeric characters? Yes or No?");
-    passwordPromts.numeric = passwordPromts.numeric.toLowerCase ();
-    if (passwordPromts.numeric === "yes") {
-        window.alert ("You have choosen to include numeric characters.");
-        passwordPromts.numeric = true
-    }
-    else if (passwordPromts.numeric === "no") {
-        window.alert ("You have choosen not to include numeric characters.")
-        passwordPromts.numeric = false
-    }
-
-    passwordPromts.symbol = window.prompt("Would you like to include symbol characters? Yes or No?");
-    passwordPromts.symbol = passwordPromts.symbol.toLowerCase ();
-    if (passwordPromts.symbol === "yes") {
-        window.alert ("You have choosen to include symbol characters.");
-        passwordPromts.symbol = true
-    }
-    else if (passwordPromts.symbol === "no") {
-        window.alert ("You have choosen not to include symbol characters.")
-        passwordPromts.symbol = false
-    }
-    window.alert("generating password.");
-};
-
-var generatePassword = function() {
-    passwordPromts.length < 8 || passwordPromts.length >128 
-    passwordPromts.length = window.prompt("Choose the length of the password ( at least 8 characters and no more than 128 characters).")
-    if (passwordPromts.length < 8 || passwordPromts.length > 128){
+ 
+ var generatePassword = function() {
+    passwordPrompts.length < 8 || passwordPrompts.length >128 
+    passwordPrompts.length = window.prompt("Choose the length of the password ( at least 8 characters and no more than 128 characters).")
+    if (passwordPrompts.length < 8 || passwordPrompts.length > 128){
         window.alert("Please enter valid length that is between 8 and 128.");
     }
     else {
-        window.alert("You have entered a length of " + passwordPromts.length + ".");
+        window.alert("You have entered a length of " + passwordPrompts.length + ".");
+    }
+    characterTypes ();
+    
+    function makePassword () {
+        var characters = "";
+        if (passwordPrompts.symbol === true)  {
+            characters = ("~","!","@","#","$","%","^","&","*","(",")","_","-","=","+","<",">",",",".","/","'","`","|")
+        }
+        if (passwordPrompts.numeric === true) {
+            characters = ("0","1","2","3","4","5","6","7","8","9")
+        }
+        if (passwordPrompts.lowercase === true) {
+            characters = ("a","b","c","e","d","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+        }
+        if (passwordPrompts.uppercase === true) {
+            characters = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+        }
+    
+        window.alert("Your password is " + result)
+        
     }
     
-    characterTypes();
-    window.alert("generating password.");
-function makePassword () {
-    var characters = "";
-    if (passwordPromts.lowercase === true) {
-       return String.fromCharCode(97 + randomNumber (0, 26));
-    }
-    if (passwordPromts.uppercase === true) {
-        return String.fromCharCode(65 + randomNumber (0, 26));
-    }
-    if (passwordPromts.numeric === true) {
-        return String.fromCharCode(49 + randomNumber (0, 9));
-    }
-    if (passwordPromts.symbol === true) {
-        return String.fromCharCode(33 + randomNumber (0, 14));
-    }
-    var results = "";
-    var length = passwordPromts.length
-    for (var i = 0, n = characters.length; i < length; i++) {
-        results += characters.charAt(Math.floor(Math.random() * n));
-    }
-    window.alert("Your new password is" + results);
-    return results;
-}
 makePassword();
 };
 
+var characterTypes = function () {
+    passwordPrompts.lowercase = window.prompt("Would you like to include lowercase characters? Yes or No?");
+    passwordPrompts.lowercase = passwordPrompts.lowercase.toLowerCase ();
+    if (passwordPrompts.lowercase === "yes") {
+        window.alert ("You have choosen to include lowercase characters.");
+        passwordPrompts.lowercase = true
+    }
+    else if (passwordPrompts.lowercase === "no") {
+        window.alert ("You have choosen not to include lowercase characters.")
+        passwordPrompts.lowercase = false
+    }
+   
+    passwordPrompts.uppercase = window.prompt("Would you like to include uppercase characters? Yes or No?");
+    passwordPrompts.uppercase = passwordPrompts.uppercase.toLowerCase ();
+    if (passwordPrompts.uppercase === "yes") {
+        window.alert ("You have choosen to include uppercase characters.");
+        passwordPrompts.uppercase = true
+    }
+    else if (passwordPrompts.uppercase === "no") {
+        window.alert ("You have choosen not to include uppercase characters.")
+        passwordPrompts.uppercase = false
+    }
 
+    passwordPrompts.numeric = window.prompt("Would you like to include numeric characters? Yes or No?");
+    passwordPrompts.numeric = passwordPrompts.numeric.toLowerCase ();
+    if (passwordPrompts.numeric === "yes") {
+        window.alert ("You have choosen to include numeric characters.");
+        passwordPrompts.numeric = true
+    }
+    else if (passwordPrompts.numeric === "no") {
+        window.alert ("You have choosen not to include numeric characters.")
+        passwordPrompts.numeric = false
+    }
+
+    passwordPrompts.symbol = window.prompt("Would you like to include symbol characters? Yes or No?");
+    passwordPrompts.symbol = passwordPrompts.symbol.toLowerCase ();
+    if (passwordPrompts.symbol === "yes") {
+        window.alert ("You have choosen to include symbol characters.");
+        passwordPrompts.symbol = true
+    }
+    else if (passwordPrompts.symbol === "no") {
+        window.alert ("You have choosen not to include symbol characters.")
+        passwordPrompts.symbol = false
+    }
+    window.alert("generating password.");
+};
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var passwordPromts = {
-    length: "",
+var passwordPrompts = {
     lowercase: "",
     uppercase: "",
     numeric: "",
-    symbol: "",
-};
-
-//Ask Criteria and length
-
+    symbol:"",
+}
 // Write password to the #password input
 function writePassword() {
 
